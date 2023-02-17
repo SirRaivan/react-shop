28d579835dd13d27ba11943b8a8ebceaa36aeaaf
SirRaivan <ivanarturo.cio@gmail.com>
2023-02-16 22:56:17 -0600

    Se agrega plugin css y stylus y su configuracion

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -27,6 +28,14 @@ module.exports = {
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ],
             }
         ]
     },
@@ -35,5 +44,13 @@ module.exports = {
             template: "./public/index.html",
             filename: "./index.html"
         }),
-    ]
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+    ],
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 3005,
+    }
 }
