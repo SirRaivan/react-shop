0c87524375d0e792500c7fcd9ef5907c4139eb5d
SirRaivan <ivanarturo.cio@gmail.com>
2023-02-20 22:36:22 -0600

    Add ReactDom

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,"dist"),
         filename: "bundle.js",
+        publicPath: "/",
     },
     mode: "development",
     resolve: {
@@ -49,8 +50,7 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: path.join(__dirname, "dist"),
-        compress: true,
-        port: 3005,
+        
+        historyApiFallback: true,
     }
 }
