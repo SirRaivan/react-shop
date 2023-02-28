a938919dc7321162ad39bd9d105448a6039adaba
SirRaivan <ivanarturo.cio@gmail.com>
2023-02-27 23:15:14 -0600

    funcion boton remover carrito

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
 const initialState = {
-    cart: [],
+	cart: [],
 }
 
 const useInitialState = () => {
-    const [state, setState] = useState(initialState);
+	const [state, setState] = useState(initialState);
 
-    const addToCart = (payload) => {
-        setState({
-            ...state,
-            cart: [...state.cart, payload]
-        });
-    };
-    
-    return {
-        state,
-        addToCart
-    }    
+	const addToCart = (payload) => {
+		setState({
+			...state,
+			cart: [...state.cart, payload]
+		});
+	};
+
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id !== payload.id),
+		});
+	}
+
+	return {
+		state,
+		addToCart,
+		removeFromCart
+	}
 }
 
 export default useInitialState;
